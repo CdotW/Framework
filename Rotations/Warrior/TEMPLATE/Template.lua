463eccff4099e9084351989d28e6382efc3a9bde
CdotW <cwhiting428@gmail.com>
2023-10-26 22:13:13 +0000

    Oct 26, 2023, 6:13 PM

--- Rotations/Warrior/TEMPLATE/Template.lua
+++ Rotations/Warrior/TEMPLATE/Template.lua
@@ -1,5 +1,5 @@
-local TMW       = TMW
-local CNDT      = TMW.CNDT
-local Env       = CNDT.Env
+local TMW               = TMW
+local CNDT              = TMW.CNDT
+local Env               = CNDT.Env
 
 local Action            = Action
