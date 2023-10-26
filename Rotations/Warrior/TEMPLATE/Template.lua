f16500f82cbd0e03e595c48a27047b944e964c69
CdotW <cwhiting428@gmail.com>
2023-10-26 22:16:24 +0000

    Oct 26, 2023, 6:16 PM

--- Rotations/Warrior/TEMPLATE/Template.lua
+++ Rotations/Warrior/TEMPLATE/Template.lua
@@ -2,5 +2,5 @@ local _G, setmetatable  = _G, setmetatable
 local TMW               = TMW
 local CNDT              = TMW.CNDT
 local Env               = CNDT.Env
-
 local Action            = Action
+local CONST             = Action.CONST
